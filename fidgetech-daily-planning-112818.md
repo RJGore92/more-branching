14414b180da64f6cbf28afc738ad455c72d0288e
Robert Gore <DarkburstGX@gmail.com>
2018-11-28 13:56:04 -0800

    Primary commit for November 28, 2018.  Contains finalized planning document for November 28, updates HTML with new forms and divs, adds scripting to proceed through and reset variables, and adds styling for spectrum ranges.

---
+++ fidgetech-daily-planning-112818.md
@@ -0,0 +1,34 @@
+# Daily Planning Doc
+
+##### Date:
+
+## BEFORE BEGINNING WORK:
+
+
+#### Please list the names of the lessons you are going to work on today:
+
+* 2.4 - More Branching
+
+
+#### Please list 3 concepts you are going to focus on today. (For example, css grids, html forms, string methods, etc.):
+
+1. JavaScript Scripting and Branching
+
+2. Dynamic CSS based on Forms and Form Inputs
+
+3. Adding and Removing divs and Classes based on Form Results
+
+
+
+## BEFORE YOU LEAVE:
+
+
+#### Please list the names of the lessons you completed today:
+
+* 2.4 - More Branching
+
+
+
+#### Please list the names of the projects you worked on today:
+
+* 2.4 - More Branching - Political Meter (scripting worked on and styles established for five spectrum ranges)
--- js/scripts2.js
+++ js/scripts2.js
@@ -1,6 +1,31 @@
-var estLoc = 0;
+var estLoc = null;
+var inputName = null;
+var inputDate = null;
+var inputGender = null;
+
 
 $(document).ready(function() {
-  
-  event.preventDefault();
+  $("form#initialForm").submit(function(event) {
+    inputName = ($("#prelimInputA1").val()).concat(" ", $("#prelimInputA2").val());
+    console.log(inputName);
+    inputDate = $("#prelimInputB").val();
+    inputGender = $("input:radio[name=gender]:checked").val();
+    $("div#formA").hide();
+    $("div#formB").show();
+    $("span#firstName").append($("#prelimInputA1").val());
+    event.preventDefault();
+  });
+  $("form#mainForm").submit(function(event) {
+    @("div#formB").hide();
+    $("div#endDiv").show();
+    event.preventDefault();
+  });
+  $("form#resetForNew").submit(function(event) {
+    inputName = null;
+    inputDate = null;
+    inputGender = null;
+    $("div#endDiv").hide();
+    $("div#formA").show();
+    event.preventDefault();
+  });
 });
